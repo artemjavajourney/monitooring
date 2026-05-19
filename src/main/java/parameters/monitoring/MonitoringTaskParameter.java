1fef6ab0f52f2486ea5df3df313ad3c4164d5d38
wayrests <av.10.000@yandex.ru>
2026-05-19 15:04:47 +0300

    Harden monitoring parameter JSON serialization

--- src/main/java/parameters/monitoring/MonitoringTaskParameter.java
+++ src/main/java/parameters/monitoring/MonitoringTaskParameter.java
@@ -1,3 +1,6 @@
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +13,8 @@ import java.util.List;
  */
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class MonitoringTaskParameter {
 
     /**
@@ -54,6 +59,7 @@ public class MonitoringTaskParameter {
      */
     private String subject;
 
+    @JsonIgnore
     public boolean isRecoveryModeEnabled() {
         return Boolean.TRUE.equals(recoveryMode);
     }
